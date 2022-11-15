8e4c9cd961ffca46ce5a3ea6e3f0fa69ad5db90b
Andrew Au <andrewau@microsoft.com>
2022-11-15 15:16:48 -0800

    AutoTrace

--- src/Tools/dotnet-trace/run.sh
+++ src/Tools/dotnet-trace/run.sh
@@ -2,5 +2,5 @@
 
 echo $USER@`hostname` "$PWD"
 echo [`date`] $ dotnet run -p dotnet-trace.csproj -c Debug --no-restore --no-build -- "$@"
-dotnet run -p dotnet-trace.csproj -c Debug --no-restore --no-build -- "$@"
+../../../dotnet.sh run -p dotnet-trace.csproj -c Debug --no-restore --no-build -- "$@"
 
---
+++ src/Tools/dotnet-trace/test.cmd
@@ -0,0 +1,2 @@
+pushd %~dp0
+call run.cmd collect --clrevents gc --clreventlevel verbose %* 
